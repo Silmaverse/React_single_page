1654f3558a5ec7d2774640fb4866cc58861c4fb9
Silmaverse <silmasubah105@gmail.com>
2025-08-23 02:09:44 +0600

    birokti

--- src/layOuts/LayOutOne.jsx
+++ src/layOuts/LayOutOne.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Outlet } from 'react-router'
-import NavBar from '../components/Navbar'
+import NavBar from '../components/NavBar'
 import ResNav from '../components/ResNav'
 
 
